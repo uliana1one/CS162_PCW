f0b1008b64eccac2d00bcf61413e0d116710f04b
Uliana1one <uliana@uni.minerva.edu>
2024-10-06 20:15:37 -0300

    React II - the tutorial

--- react-microblog/src/components/Posts.js
+++ react-microblog/src/components/Posts.js
@@ -1,38 +1,58 @@
-export default function Posts() {
-    const posts = [
-      {
-        id: 1,
-        text: 'Hello, world!',
-        timestamp: 'a minute ago',
-        author: {
-          username: 'susan',
-        },
-      },
-      {
-        id: 2,
-        text: 'Second post',
-        timestamp: 'an hour ago',
-        author: {
-          username: 'john',
-        },
-      },
-    ];
-  
-    return (
-      <>
-        {posts.length === 0 ?
-          <p>There are no blog posts.</p>
-        :
-          posts.map(post => {
-            return (
-              <p key={post.id}>
-                <b>{post.author.username}</b> &mdash; {post.timestamp}
-                <br />
-                {post.text}
-              </p>
-            );
-          })
-        }
-      </>
-    );
+
+import { useState, useEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import { useApi } from '../contexts/ApiProvider';
+import Post from './Post';
+
+export default function Posts({ content }) {
+  const [posts, setPosts] = useState();
+  const api = useApi();
+
+  let url;
+  switch (content) {
+    case 'feed':
+    case undefined:
+      url = '/feed';
+      break;
+    case 'explore':
+      url = '/posts';
+      break
+    default:
+      url = `/users/${content}/posts`;
+      break;
   }
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get(url);
+      if (response.ok) {
+        setPosts(response.body.data);
+      }
+      else {
+        setPosts(null);
+      }
+    })();
+  }, [api, url]);
+
+  return (
+    <>
+      {posts === undefined ?
+        <Spinner animation="border" />
+      :
+        <>
+          {posts === null ?
+             <p>Could not retrieve blog posts.</p>
+          :
+            <>
+              {posts.length === 0 ?
+                <p>There are no blog posts.</p>
+              :
+                posts.map(post => <Post key={post.id} post={post} />)
+              }
+            </>
+          }
+        </>
+      }
+    </>
+  );
+}
